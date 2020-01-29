33884f0afdfddad9fbef81b1059c01f957398e09
Ghada Dridi <35421354+GhadaRV@users.noreply.github.com>
2020-01-29 22:35:00 +0100

    First Commit

---
+++ Assets/Script/ProfiloAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfiloAnim : MonoBehaviour
+{
+    Animator anim;
+    void Start()
+    {
+        anim=GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+		{
+			anim.SetTrigger("profilo");
+		}
+    }
+}
